36980fda14ac0e0d1076fc57fe29636078610bd5
Caio Souza <caiog.valim@outlook.com>
2026-05-17 15:36:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ botaoAnterior.onclick = function voltarSlider(){
     listaImagens[contador].classList.add("ativo")
 }
 
+//fim carrossel de imagens
 
 const textoElemento = document.querySelector(".texto");
 const frases = ["Impressora 3D", "Super Rápida", "Baixo Nível de Ruído", "Alta Performace", "Firmaware Marlin", "Ideal para PETG, PLA, TPU"];
@@ -69,6 +70,7 @@ function digitar() {
 // Inicia a função
 digitar();
 
+//fim texto digitado
 
 
 const observadorNomes = new IntersectionObserver((entradas) => {
@@ -86,3 +88,47 @@ const observadorNomes = new IntersectionObserver((entradas) => {
 const nomesEquipe = document.querySelectorAll('.naovisivel')
 
 nomesEquipe.forEach((nomeEquipe) => observadorNomes.observe(nomeEquipe))
+
+//fim nomes equipe
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+
+});
+
+//fim cursor personalizado
+
+const observadorLista = new IntersectionObserver((entradas) => {
+  entradas.forEach((entry) => {
+    if(entry.isIntersecting){
+        entry.target.classList.add('visivel')
+        entry.target.classList.remove('naovisivel2')
+    }else{
+        entry.target.classList.remove('visivel')
+        entry.target.classList.add('naovisivel2')  
+    }
+  })
+})
+
+const listaMateriais = document.querySelectorAll('.naovisivel2')
+
+listaMateriais.forEach((lista) => observadorLista.observe(lista))
+
+//fim Lista de Materiais
+
+const sombotaoAnterior = document.getElementById("somBotaoAnterior");
+const sombotaoProximo = document.getElementById("somBotaoProximo");
+const som = document.getElementById("som");
+
+sombotaoAnterior.addEventListener("click", () => {
+  som.currentTime = 0;
+  som.play();
+});
+
+sombotaoProximo.addEventListener("click", () => {
+  som.currentTime = 0;
+  som.play();
+});
